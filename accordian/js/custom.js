85aafe5f26bf172fd32bce56a7d7f065de8f2335
ahmed mohsen <ahmed.m.abdelmaged90@gmail.com>
2017-03-25 22:12:41 +0200

    creating image accordian

---
+++ accordian/js/custom.js
@@ -0,0 +1,10 @@
+/*type script*/
+
+$(function(){
+		$(".movable-text").typed({
+			strings: ["Thanks Egypt "],
+			typeSpeed: 700,
+            loop: true,
+            showCursor:false,
+		});
+	});
